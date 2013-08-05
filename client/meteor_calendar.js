b07a4bca48c0f80be3d44c343c818c6d7d10acb9
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-08-05 17:09:59 -0300

    Added mesosphere package for forms validation

--- client/meteor_calendar.js
+++ client/meteor_calendar.js
@@ -38,27 +38,6 @@ Template.calendar.events({
 });
 
 Template.calendar_modal.events({
-    'submit form': function (event) {
-        event.preventDefault();
-        return false;
-    },
-    'click button#save-event' : function (event) {
-        var $modal = $('#new-event-modal'),
-            $form = $modal.find('form'),
-            formValues = {allDay: false};
-        
-        formValues.start = $form.find('input[name=date]').val();
-        formValues.title = $form.find('input[name=title]').val();
-        if ($form.find('input[type=radio]:checked').val() === 'no-time') {
-            formValues.allDay = true;
-        } else {
-            formValues.start = formValues.start + ' ' + $form.find('input[name=time-value]').val();
-        }
-        formValues.start = moment(formValues.start.trim(), 'YYYY-MM-DD HH:mm').toDate();
-
-        console.log(formValues)
-        CalendarFacade.addEvent(formValues);        
-    },
     'click input[type=radio]' : function (event) {
         if (event.target.value === 'has-time') {
             $('#time-input').show();
