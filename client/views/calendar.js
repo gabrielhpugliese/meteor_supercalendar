b9dcca892367e720e5167356363fa2f96542c662
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-08-08 10:50:06 -0300

    Adding fork-me and madewith

--- client/views/calendar.js
+++ client/views/calendar.js
@@ -1,4 +1,12 @@
 if (Meteor.isClient) {
+    new ForkMe({
+      user: 'gabrielhpugliese',
+      repo: 'meteor_supercalendar',
+      ribbon: {
+          color: 'darkblue',
+          position: 'left'
+        }
+    });
 }
 
 if (Meteor.isServer) {
