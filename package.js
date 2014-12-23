9ae2b334292fd2bf62fe93b3f07637d4035e8a90
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2014-12-23 11:20:08 -0200

    Upgrade to METEOR@0.9.2+

--- package.js
+++ package.js
@@ -1,53 +1,56 @@
 Package.describe({
-    summary: "Google Calendar-like as smart package"
+  name: "gabrielhpugliese:supercalendar",
+  summary: "Google Calendar-like as smart package",
+  version: "0.3.0",
+  git: "https://github.com/gabrielhpugliese/meteor_supercalendar"
 });
 
-Package.on_use(function (api, where) {
-    // Client
-    api.use([
-            'standard-app-packages',
-            'meteor',
-            'startup',
-            'templating',
-            'session',
-            'deps',
-            'preserve-inputs',
-            'jquery',
-            'bootstrap',
-            'less',
-            'accounts-ui'
-    ], 'client');
-    api.add_files([
-                  'client/stylesheets/calendar.less',
-                  'client/stylesheets/fullcalendar.css',
-                  'client/stylesheets/fullcalendar.print.css',
-                  'client/stylesheets/jquery-ui-1.10.3.custom.css',
-                  'client/views/calendar.html',
-                  'client/views/calendar.js'
-    ], 'client');
-    api.add_files([
-                  'client/compatibility/fullcalendar.js',
-                  'client/compatibility/jquery.livequery.js',
-                  'client/compatibility/jquery-ui-1.10.3.custom.js'
-    ], 'client', {raw: true});
+Package.onUse(function (api, where) {
+  api.versionsFrom('METEOR@0.9.2');
 
-    // Both
-    api.use([
-            'Mesosphere', 
-            'underscore'
-    ], ['client', 'server']);
-    api.add_files([
-                  'lib/models.js', 
-                  'lib/forms.js'
-    ], ['client', 'server']);
+  // Other packages
+  api.use('copleykj:mesosphere@0.1.14');
+  api.use('bootstrap@1.0.1');
+  // Client
+  api.use([
+    'startup',
+    'templating',
+    'session',
+    'tracker',
+    'preserve-inputs',
+    'jquery',
+    'less',
+  ], 'client');
+  api.add_files([
+    'client/stylesheets/calendar.less',
+    'client/stylesheets/fullcalendar.css',
+    'client/stylesheets/fullcalendar.print.css',
+    'client/stylesheets/jquery-ui-1.10.3.custom.css',
+    'client/views/calendar.html',
+    'client/views/calendar.js'
+  ], 'client');
+  api.add_files([
+    'client/compatibility/fullcalendar.js',
+    'client/compatibility/jquery.livequery.js',
+    'client/compatibility/jquery-ui-1.10.3.custom.js'
+  ], 'client', {raw: true});
 
-    // Server
-    api.add_files([
-                  'server/publications.js'
-    ], 'server');
+  // Both
+  api.use([
+    'underscore'
+  ], ['client', 'server']);
+  api.add_files([
+    'lib/models.js',
+    'lib/forms.js'
+  ], ['client', 'server']);
 
-    if (typeof api.export !== 'undefined') {
-        api.export('Calendar', ['client', 'server']);
-    }
+  // Server
+  api.add_files([
+    'server/publications.js'
+  ], 'server');
+
+  if (typeof api.export !== 'undefined') {
+    api.export('Calendar', ['client', 'server']);
+  }
 
 });
