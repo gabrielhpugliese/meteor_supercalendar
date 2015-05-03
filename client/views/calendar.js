76ac2c12d5a7e8bc395a2e79835cef0a015b0b19
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-05-03 18:00:37 -0300

    Add back the event to know the time - v0.7.1

--- client/views/calendar.js
+++ client/views/calendar.js
@@ -1,3 +1,15 @@
 Template.calendar.rendered = function () {
   return SuperCalendar.rendered.call(this);
 };
+
+Template.new_event_modal.events({
+  'click input[type=radio]': function (event, t) {
+    var $timeInput = t.$('#time-input');
+
+    if (event.target.value === 'has-time') {
+      return $timeInput.show();
+    }
+
+    return $timeInput.hide();
+  }
+});
